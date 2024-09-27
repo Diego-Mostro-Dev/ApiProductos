93c091ede909af4d96e561759e8ee17a135ee218
Diego Alejandro Salvado <diegosalvadodev@gmail.com>
2024-09-27 18:52:39 -0300

    Añadir configuración de archivos estáticos

--- apirest/settings.py
+++ apirest/settings.py
@@ -156,16 +156,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# This setting informs Django of the URI path from which your static files will be served to users
-# Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Asegúrate de que esto esté siempre configurado
 
-# This production code might break development mode, so we check whether we're in DEBUG mode
+# Asegúrate de que el directorio existe
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Verifica que esta carpeta exista
+
+# Configuración de WhiteNoise
 if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
@@ -185,4 +184,3 @@ cloudinary.config(
 
 
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
