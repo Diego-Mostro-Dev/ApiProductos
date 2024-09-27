ba5f042c1d35323d3c9c24a44199c52c9d2ce3a3
Diego Alejandro Salvado <diegosalvadodev@gmail.com>
2024-09-27 18:45:48 -0300

    corregi el link de cloudinary

--- api/models.py
+++ api/models.py
@@ -1,12 +1,23 @@
-
+import cloudinary.uploader
 from django.db import models
-from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     description = models.TextField()
-    image = CloudinaryField('image', null=True, blank=True ,resource_type='image')  # Usar Cloudinary para imágenes
+    image = models.URLField(max_length=500, null=True, blank=True)  # Usar URLField para la URL de la imagen
 
+    def save(self, *args, **kwargs):
+        if self.image:  # Solo si hay una imagen
+            # Crear un public_id personalizado
+            public_id = f"mi_prefijo_{slugify(self.name)}"
+            # Subir la imagen manualmente a Cloudinary con el public_id
+            upload_data = cloudinary.uploader.upload(self.image, public_id=public_id)
+            # Guardar la URL completa de la imagen subida en el campo image
+            self.image = upload_data['secure_url']  # Almacenar la URL segura completa
+        
+        super(Product, self).save(*args, **kwargs)
+    
     def __str__(self):
         return self.name
